14c4f2f22adc83d953d149bd34308099878d1cfd
Bridget Johnson <johnsonbridget.d@gmail.com>
2015-10-26 15:52:12 +1300

    loading the new background image

--- tactile.motion_v2/GameViewController.swift
+++ tactile.motion_v2/GameViewController.swift
@@ -10,44 +10,32 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
-    }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      let scene = GameScene(size:CGSize(width: 1536, height: 2048))
+      // Configure the view.
+      let skView = self.view as! SKView
+      skView.showsFPS = false
+      skView.showsNodeCount = false
+      
+      /* Sprite Kit applies additional optimizations to improve rendering performance */
+      skView.ignoresSiblingOrder = true
+      
+      /* Set the scale mode to scale to fit the window */
+      scene.scaleMode = .AspectFill
+      
+      skView.presentScene(scene)
+      
+   }
+   override func prefersStatusBarHidden() -> Bool {
+      return true
+   }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
 }
